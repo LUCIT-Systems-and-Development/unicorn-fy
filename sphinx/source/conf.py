f6392b00e876b24472cf4e6fda58173b78e68428
Oliver Zehentleitner <oliver.zehentleitner@unicorn-data.com>
2019-11-04 15:44:41 +0100

    update docs

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2019, Oliver Zehentleitner'
 author = 'Oliver Zehentleitner'
 
 # The full version, including alpha/beta/rc tags
-release = '0.3.0'
+release = '0.3.1'
 
 
 # -- General configuration ---------------------------------------------------
