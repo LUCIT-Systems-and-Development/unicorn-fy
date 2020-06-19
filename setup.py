c53ce389915441378a28f50d88aac42692a0f2a9
Oliver Zehentleitner <oliver.zehentleitner@unicorn-data.com>
2020-06-19 20:12:44 +0200

    travis ci

--- setup.py
+++ setup.py
@@ -58,7 +58,7 @@ setuptools.setup(
      packages=setuptools.find_packages(),
      classifiers=[
          "Development Status :: 5 - Production/Stable",
-         "Programming Language :: Python :: 2",
+         "Programming Language :: Python :: 2.7",
          "Programming Language :: Python :: 3",
          "License :: OSI Approved :: MIT License",
          'Intended Audience :: Developers',
