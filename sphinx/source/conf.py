db42494ea4fd76ad0a0d6e9cbd6990d6ec81583e
oliver <oliver@zehentleitner.co>
2020-10-03 01:08:45 +0200

    docs

--- sphinx/source/conf.py
+++ sphinx/source/conf.py
@@ -17,7 +17,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'UnicornFy'
+project = 'unicorn_fy'
 copyright = '2019-2020, Oliver Zehentleitner'
 author = 'Oliver Zehentleitner'
 
