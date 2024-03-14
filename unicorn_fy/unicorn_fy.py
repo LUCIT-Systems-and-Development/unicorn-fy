75c71fadd94e1db07ed693f90e89d7b353ba69bc
Oliver Zehentleitner <47597331+oliver-zehentleitner@users.noreply.github.com>
2024-03-14 11:53:03 +0100

    Update unicorn_fy.py

--- unicorn_fy/unicorn_fy.py
+++ unicorn_fy/unicorn_fy.py
@@ -63,7 +63,7 @@ class UnicornFy(object):
         - Binance.org
         - Jex.com
     """
-    VERSION = "1.14.1.dev"
+    VERSION = "1.14.1"
 
     def __init__(self, debug=False):
         self.last_update_check_github = {'timestamp': time.time(),
